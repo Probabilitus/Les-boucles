3de4ab693b062e832afa53018cf91abdf45bbd15
Ab <meteoritus75@gmail.com>
2024-04-15 00:41:45 +0200

    Challenge les boucles

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Les_boucles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] moves = new string[32];
+            
+            for (int i = 0; i < moves.Length; i++)
+            {
+                Console.WriteLine("Veuillez entrer un pas de danse: ");
+                string enterStep = Console.ReadLine();
+                string stapeDanceUser = moves[i];
+                moves.Append(stapeDanceUser);
+              
+                Console.WriteLine(moves);
+                if (enterStep == "done") 
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
